e8c6289d64982558f546916dcf8e15cd99dcd8ab
Sachin.g.s <Sachin.g.s@DESKTOP-304V0E1>
2022-09-23 11:11:29 +0530

    first commit

---
+++ src/test/resources/PractiesTest.java
@@ -0,0 +1,9 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PractiesTest {
+@Test
+ public void practies() {
+	Reporter.log("practies1");
+}
+}
---
+++ src/test/resources/practies2Test.java
@@ -0,0 +1,10 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class practies2Test {
+@Test
+public void practies2Test() {
+	System.out.println("practies2Test");
+	
+}
+}
