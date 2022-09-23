9f03420958f0c28da41e4f6d34e3ab0ea2b79aff
Sachin.g.s <Sachin.g.s@DESKTOP-304V0E1>
2022-09-23 15:10:25 +0530

    second commit

---
+++ src/test/resources/practices3Test.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class practices3Test {
+	@Test
+	public void practices() {
+	 String browser =System.getProperties("br");
+	 String url = System.getProperties("url");
+	 System.out.println("browser");
+	 System.out.println("url");
+	 
+	}
+
+}
